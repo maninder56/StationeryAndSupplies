6c1433f89ca14f3a8e24f31c1eaafe568d13398e
Maninder Singh <maninder123450@gmail.com>
2025-06-08 11:44:31 +0100

    created category list for first section and added some styles for first section

--- StationeryAndSuppliesWebApp/Pages/Index.cshtml.cs
+++ StationeryAndSuppliesWebApp/Pages/Index.cshtml.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using StationeryAndSuppliesWebApp.Models;
-using StationeryAndSuppliesWebApp.Services; 
+using StationeryAndSuppliesWebApp.Services;
+using System.Collections.Generic;
 
 namespace StationeryAndSuppliesWebApp.Pages; 
 
@@ -18,21 +19,44 @@ public class IndexModel : PageModel
     }
 
     // Properties shared with view 
-    public List<ChildCategory> ChildCategoriesList { get; private set; } = new List<ChildCategory>();
+    public List<ChildCategory> ChildCategoriesListInFirstSection { get; private set; } = new List<ChildCategory>();
+    public List<ChildCategory> ChildCategoriesListInSecondSection { get; private set; } = new List<ChildCategory>();
 
 
-    public async Task<IActionResult> OnGet()
+    // Page Handlers
+    public async Task<IActionResult> OnGetAsync()
     {
         logger.LogInformation("Index page requested");
 
-        ChildCategoriesList = await productInformationService.GetAllChildCategoriesAsync();
+        List<ChildCategory> list = await productInformationService.GetAllChildCategoriesAsync();
 
-        if (ChildCategoriesList.Count == 0)
+        if (list.Count == 0)
         {
             logger.LogWarning("Failed to get any category from service"); 
+            return Page();
         }
 
-        logger.LogInformation("Loaded {NumberOfChildCategories} child categories", ChildCategoriesList.Count);
+        logger.LogInformation("Total {NumberOfChildCategories} child categories loaded in list", list.Count);
+
+        if (list.Count > 8)
+        {
+            ChildCategoriesListInFirstSection = list.Slice(0, 4);
+            ChildCategoriesListInSecondSection = list.Slice(4, 4);
+
+            logger.LogInformation("Loaded {CategoryNumber} categories in ChildCategoriesListInFirstSection list", ChildCategoriesListInFirstSection.Count);
+            logger.LogInformation("Loaded {CategoryNumber} categories in ChildCategoriesListInSecondSection list", ChildCategoriesListInSecondSection.Count);
+        }
+
+        if (ChildCategoriesListInFirstSection.Count == 0)
+        {
+            logger.LogWarning("Failed to load categoreis in Firstsection list"); 
+        }
+
+        if (ChildCategoriesListInSecondSection.Count == 0)
+        {
+            logger.LogWarning("Failed to load categoreis in Secondsection list");
+        }
+
         return Page();
     }
 }
