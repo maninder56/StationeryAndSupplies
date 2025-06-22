a57b0b24eb8ad70ab114bca3863defb5170949a3
Maninder Singh <maninder123450@gmail.com>
2025-06-22 17:00:53 +0100

    Added profile navigation layout and replaces profile page with user details

---
+++ StationeryAndSuppliesWebApp/Pages/Account/UserDetails.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StationeryAndSuppliesWebApp.Pages.Account; 
+
+public class UserDetailsModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
