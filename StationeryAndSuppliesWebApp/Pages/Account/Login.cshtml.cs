2ee51ac178364080e4007e0d3a0b748bd2e2ae9e
Maninder Singh <maninder123450@gmail.com>
2025-06-23 12:13:17 +0100

    Added styles for log in page

--- StationeryAndSuppliesWebApp/Pages/Account/Login.cshtml.cs
+++ StationeryAndSuppliesWebApp/Pages/Account/Login.cshtml.cs
@@ -92,9 +92,11 @@ public class LoginModel : PageModel
     {
         [Required]
         [EmailAddress]
+        [StringLength(100)]
         public string Email { get; set; } = string.Empty;
 
         [Required]
+        [StringLength(100)]
         public string Password { get; set; } = string.Empty;
     }
 }
